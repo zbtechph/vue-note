82154867778c30de93093d9bc67375a8b13c362f
zbtechph <zbtech@1fidel.tech>
2020-06-13 11:53:35 +0800

    new: use tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    purge: [
+        './src/*.vue',
+        './src/views/*.vue',
+        './src/views/**/*.vue',
+        './src/components/*.vue',
+        './src/components/**/*.vue'
+    ],
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+}
